a5669008b445eef9d66e74e54b6c8133dab60cfa
Yongo David <davidyongo9@gmail.com>
2023-12-24 04:29:42 +0100

    working on drop down and button animation

---
+++ index.js
@@ -0,0 +1,18 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//   const userSection = document.getElementsByClassName('user-section');
+//   const arrowIcon = document.getElementsByClassName('arrow-icon');
+//   const dropdown = document.getElementsByClassName('dropdown');
+
+//   userSection.addEventListener('click', function() {
+//     dropdown.classList.toggle('hidden');
+//     arrowIcon.classList.toggle('rotated');
+//   });
+
+//   // Close the dropdown if clicked outside of it
+//   document.addEventListener('click', function(event) {
+//     if (!userSection.contains(event.target)) {
+//       dropdown.classList.add('hidden');
+//       arrowIcon.classList.remove('rotated');
+//     }
+//   });
+// });
