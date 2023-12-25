c220b7fb149ac92678de07701bc7360006f8db86
Yongo David <davidyongo9@gmail.com>
2023-12-25 14:21:54 +0100

    trying to make the signup page more dynamic so it can store the form data

--- SignupFiles/Signup.js
+++ SignupFiles/Signup.js
@@ -17,28 +17,68 @@
 
 
 
+// document.addEventListener('DOMContentLoaded', function () {
+//     const processForm = () => {
+//         let firstname = document.getElementById('Fname').value;
+//         let lastname = document.getElementById('Lname').value;
+//         let email = document.getElementById('email').value;
+//         let password = document.getElementById('Password').value;
+//         let conPassword = document.getElementById('password').value; // Corrected the ID
+
+//         // Call the displayData function with the collected data
+//         displayData(firstname, lastname, email, password, conPassword);
+//     };
+
+//     function displayData(firstname, lastname, email, password, conPassword) {
+//         console.log('Signup complete');
+//         console.log('First Name:', firstname);
+//         console.log('Last Name:', lastname);
+//         console.log('Email:', email);
+//         console.log('Password:', password);
+//         console.log('Confirm Password:', conPassword);
+//     }
+
+//     // Use querySelector to select the element by class
+//     let submit = document.querySelector('.Formbtn');
+//     submit.addEventListener('click', processForm);
+// });
+
 document.addEventListener('DOMContentLoaded', function () {
     const processForm = () => {
         let firstname = document.getElementById('Fname').value;
         let lastname = document.getElementById('Lname').value;
         let email = document.getElementById('email').value;
         let password = document.getElementById('Password').value;
-        let conPassword = document.getElementById('password').value; // Corrected the ID
+        let conPassword = document.getElementById('password').value;
 
-        // Call the displayData function with the collected data
-        displayData(firstname, lastname, email, password, conPassword);
+        displayData(firstname, lastname, email, password, conPassword); {
+            console.log('Submitted')
+        }
+        
+        let formData = {
+            Firstname: firstname,
+            lastname: lastname,
+            email: email,
+            password: password,
+            conPassword: conPassword
+        };
+        localStorage.setItem('formData', JSON.stringify(formData))
     };
+    processForm()
 
-    function displayData(firstname, lastname, email, password, conPassword) {
-        console.log('Signup complete');
-        console.log('First Name:', firstname);
-        console.log('Last Name:', lastname);
-        console.log('Email:', email);
-        console.log('Password:', password);
-        console.log('Confirm Password:', conPassword);
+    // function displayData(firstname, lastname, email, password, conPassword) {
+    //     console.log('SIgnup comolete')
+    // }
+
+    function displayData() {
+        let GetStoreData = localStorage.getItem('formData');
+
+        let formData = JSON.parse(GetStoreData);
+
+        console.log('Stored Data' ,formData);
     }
 
-    // Use querySelector to select the element by class
-    let submit = document.querySelector('.Formbtn');
-    submit.addEventListener('click', processForm);
-});
+    
+    let submit = document.querySelector('.Formbtn')
+    submit.addEventListener('click',processForm)
+})
