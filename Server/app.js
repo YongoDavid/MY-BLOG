6326f0ca60468c8086d876fffe520c71ef4728d5
Yongo David <davidyongo9@gmail.com>
2024-01-13 16:18:08 +0100

    update

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,12 @@
 const http = require('http')
-
+q
 const server = http.createServer((req,res) =>{
     res.setHeader('Content-Type' , 'Text/plain')
     res.writeHead(200)
     res.write('welcome')
     res.end()
 })
+
 
 server.listen(4000 , 'localhost' ,() =>{
     console.log('listeing on port 4000')
