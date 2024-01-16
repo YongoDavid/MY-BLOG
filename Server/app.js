f99bd614abfdac7fa12b19121baae6ebd4be3e86
Yongo David <davidyongo9@gmail.com>
2024-01-16 23:41:23 +0100

    running server tests

--- Server/app.js
+++ Server/app.js
@@ -2,7 +2,7 @@ const http = require('http')
 const server = http.createServer((req,res) =>{
     res.setHeader('Content-Type' , 'Text/plain')
     res.writeHead(200)
-    res.write('welcome')
+    res.write('this is the homee page')
     res.end()
 })
 
@@ -10,3 +10,5 @@ const server = http.createServer((req,res) =>{
 server.listen(3000 , 'localhost' ,() =>{
     console.log('listeing on port 3000')
 })
+
+
